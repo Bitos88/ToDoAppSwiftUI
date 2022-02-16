3ab3ea1813c984d4aff033ad1f556aa28d861e2c
Bitos88 <tyrion72@gmail.com>
2022-02-16 10:41:01 +0100

    Initial Commit

---
+++ TodoAppSwiftUI/TodoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppSwiftUIApp.swift
+//  TodoAppSwiftUI
+//
+//  Created by Alberto Alegre Bravo on 16/2/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
